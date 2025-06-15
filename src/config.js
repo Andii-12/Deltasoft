6e477b2af5004ecb7f3e8ddf06a29598ff45b98b
ELVN <60426649+Andii-12@users.noreply.github.com>
2025-06-15 18:31:04 +0800

    config change

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const config = {
   API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5001',
-  ENV: process.env.NODE_ENV || 'development',
-  RAILWAY_URL: 'https://deltasoft-production.up.railway.app'
+  ENV: process.env.NODE_ENV || 'development'
 };
 
 export default config; 
