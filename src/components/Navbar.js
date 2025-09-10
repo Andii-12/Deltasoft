83955a8a212e96fce4d55d1b52001f7f66b42c17
Your Name <jackpountjay@gmail.com>
2025-09-10 15:20:40 +0800

    up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center space-x-3" onClick={handleNavClick('top')}>
             <img
-              src="/images/Logo/Logo2.svg"
+              src={`/images/Logo/Logo2.svg?v=${Date.now()}`}
               alt="DeltaSoft Logo"
               className="h-8 w-auto"
               onError={(e) => {
